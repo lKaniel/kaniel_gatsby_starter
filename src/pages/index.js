c1f263bb9d0b46a42995d7e66cdd03a61c17782e
Kaniel <vitshev120@gmail.com>
2022-01-01 18:26:54 +0200

    Update 1.1.0
    - Refactored all saga's code
    - Added saga for media queries
    - Configured new plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
 import React from "react"
-import Layout from "../components/Layout/Layout";
 import Seo from "../components/Seo";
 
 export default function Home() {
 
     return (
-        <Layout>
+        <>
             <Seo title={"home"}/>
             <h2>
                 Hello from Kaniel's starter!
             </h2>
-        </Layout>
+        </>
     )
 }
